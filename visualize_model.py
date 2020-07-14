bdd9535b94d29ab3002c27b46c243b1df93c9296
Gao JiaXiang <gaojxcs@gmail.com>
2020-07-14 17:59:31 +0800

    action visualization

--- visualize_model.py
+++ visualize_model.py
@@ -14,12 +14,50 @@ def draw_matrix(data, fig, ax):
     psm = ax.pcolormesh(data, rasterized=True)
     fig.colorbar(psm, ax=ax)
 
+
 def put_text_on_matrix(data, fig, ax):
     xdim = data.shape[0]
     ydim = data.shape[1]
     for xi in range(xdim):
         for yi in range(ydim):
             data[xi][yi]
+
+
+def majority_action(action_list):
+    b = np.bincount(action_list)
+    return np.argmax(b)
+
+
+def action_along_value(q_table, action_axis, feature_names=None):
+    """
+    Result: 
+        position 0, 1, 2, 3, 4
+        action   0, 0, 1, 1, 2
+        action - majority action taken
+
+    Input: 
+        q_table: (dim1, dim2, dim3, action_dim)
+    """
+    action_table = np.apply_along_axis(np.argmax, axis=action_axis, arr=q_table)
+    feature_num = action_table.ndim
+
+    # initialize feature names if is None
+    if feature_names is None:
+        feature_names = list(range(feature_num))
+
+    # apply_function, flatten + majority
+    def _flatten_majority(arr):
+        if arr.ndim > 1:
+            arr_1d = np.ndarray.flatten(arr)
+        else:
+            arr_1d = arr
+        return majority_action(arr_1d)
+
+    for feature_axis in range(feature_num):
+        action_along_feature = np.apply_along_axis(
+            _flatten_majority, axis=feature_axis, arr=action_table)
+
+    pass
 
 
 def main():
