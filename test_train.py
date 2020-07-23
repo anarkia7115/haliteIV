0e46d8c5c53c916f6602edbcfcfc62aa16bc772a
Gao JiaXiang <gaojxcs@gmail.com>
2020-07-23 08:36:21 +0800

    draw nn

--- test_train.py
+++ test_train.py
@@ -4,9 +4,13 @@ from sklearn.metrics import precision_score
 import numpy as np
 import torch
 from torch import nn
+from torch.utils.tensorboard import SummaryWriter
+from torch.utils.data import random_split
 
+from dql_model import QNet
 from dql_model import DataLoader
 from visualize_model import visualize_q_net
+from datasets import CircleDataset
 
 
 def target_func(k, b):
@@ -85,27 +89,6 @@ def test_train_linear():
     fig.suptitle(f"k:{k}, b:{b}")
     plt.show()
 
-def circle_data_generator():
-    """
-        0 - in circle
-        1 - on circle
-        2 - out circle
-        radius = 0.8
-    """
-    width = 0.3
-    centerx = centery = center = 2
-    radius = center * 1.5
-    xx = np.random.randn(1000, 2)*radius + center
-    def _func_y(point):
-        if (point[0] - centerx)**2 + (point[1] - centery) **2 < (radius-width)**2:
-            return 0
-        elif (point[0] - centerx)**2 + (point[1] - centery) **2 < (radius+width)**2:
-            return 1
-        else:
-            return 2
-    yy = np.apply_along_axis(_func_y, axis=1, arr=xx)
-    return xx, yy
-
 
 def create_data_mesh(xx):
     range_x = (
@@ -125,6 +108,82 @@ def create_data_mesh(xx):
     return np.stack([X, Y], axis=-1)
 
 
+def draw_layer(layer, mesh, axs):
+    neuron_id = 0
+    for ax in axs:
+        ax.imshow(
+            layer(torch.Tensor(mesh)
+            )[...,neuron_id].detach().numpy(), 
+            aspect="auto")
+        neuron_id += 1
+
+
+def test_draw_layer():
+    print("initializing q_net")
+    q_net = QNet(2, 3)
+    print("generating data")
+    ds = CircleDataset()
+    xx = ds.xx
+    print("creating data mesh")
+    mesh = create_data_mesh(xx)
+
+    print("initialize gridspec")
+    plot0 = plt.figure(0, figsize=(30, 30))
+
+    gs = plot0.add_gridspec(20, 20)
+    gs.update(wspace=0, hspace=0) # set the spacing between axes. 
+
+    print("start plotting")
+    draw_model_layers(q_net, mesh, plot0, gs)
+
+    plot0.tight_layout()
+
+    plt.waitforbuttonpress()
+
+
+def create_gs(plot_id, rows, cols):
+    plot = plt.figure(plot_id, figsize=(20, 20))
+
+    gs = plot.add_gridspec(rows, cols)
+    gs.update(wspace=0, hspace=0) # set the spacing between axes. 
+
+    return plot, gs
+
+
+def draw_model_layers(model, mesh, ax_mat, layer_ids):
+
+    for axs, layer_id in zip(ax_mat, layer_ids):
+        layer = model.pipe[:layer_id]
+        draw_layer(layer, mesh, axs)
+
+
+def init_model_layers_axs(model, plot, gs, layer_ids):
+    """
+    1. create gridspec
+        fig size:  layer_num * max(neuron_num)
+    2. loop through pipe
+    3. loop through neuron
+    4. imshow neuron
+    """
+
+    ax_mat = []
+    for layer_id in layer_ids:
+
+        layer = model.pipe[:layer_id]
+
+        axs = [
+            plot.add_subplot(gs[layer_id, i])
+            for i in range(layer[-1].out_features) ]
+
+        # turn axis off
+        for ax in axs:
+            ax.axis('off')
+
+        ax_mat.append(axs)
+
+    return ax_mat
+
+
 def test_train_circle():
     """
     1. generate circle data
@@ -137,14 +196,24 @@ def test_train_circle():
     """
 
     # generate data
-    xx, yy = circle_data_generator()
+    ds = CircleDataset()
+
+    mesh = create_data_mesh(ds.xx)
+    train_size = int(len(xx) * 0.8)
+    test_size = len(xx) - train_size
+
+    ds = random_split(torch.stack([xx, yy], -1, [train_size, test_size]))
+    # xx = ds[0].dataset[...,0]
+    # xx = ds[0].dataset[...,0]
+
+    # random split
+    random_split()
 
     # show circle data
     fig2 = plt.figure(2, figsize=(10, 10))
     plt.scatter(xx[...,0], xx[...,1], c=yy)
     plt.pause(0.1)
 
-    from dql_model import QNet
     q_net = QNet(2, 3)
 
     dl = DataLoader(xx, yy, batch_size=256)
@@ -152,9 +221,17 @@ def test_train_circle():
     optimizer = torch.optim.SGD(q_net.parameters(), lr=0.001)
     loss_func = nn.NLLLoss()
 
+    # init grids for nn visualization
+    plot3, gs = create_gs(3, 10, 10)
+
+    layer_ids = [1, 3, 5, 7]
+    ax_mat = init_model_layers_axs(q_net, plot3, gs, layer_ids)
+
     print(q_net)
 
-    for i in range(1000):
+    writer = SummaryWriter(log_dir="runs")
+
+    for i in range(10000):
         x_batch, y_batch = dl.next_batch()
         # plt.scatter(np.array(x_batch)[...,0], np.array(x_batch)[...,1], c=y_batch)
         y_lsm = q_net(torch.Tensor(x_batch))
@@ -163,15 +240,25 @@ def test_train_circle():
         loss = loss_func(y_lsm, torch.tensor(y_batch, dtype=torch.int64))
         loss.backward()
         optimizer.step()
+        writer.scalar('loss', loss)
 
-        if i % 10 == 0:
+        precision = precision_score(
+            y_batch, 
+            q_net(torch.Tensor(x_batch)).argmax(dim=-1), 
+            average='micro')
+
+        if i % 100 == 0:
             print(i)
             print(f"loss: {loss}")
-            precision = precision_score(
-                y_batch, 
-                q_net(torch.Tensor(x_batch)).argmax(dim=-1), 
-                average='micro')
             print(f"precision: {precision}")
+
+            draw_model_layers(q_net, mesh, ax_mat, layer_ids)
+            plt.pause(0.1)
+
+            for axs in ax_mat:
+                for ax in axs:
+                    ax.cla()
 
 if __name__ == "__main__":
     test_train_circle()
+    # test_draw_layer()
