79b83ed4ad03d3b06a36cd5e5daa1f8b00ee256e
gaojx <gaojxcs@gmail.com>
2020-07-25 09:42:55 +0800

    fix data

--- datasets.py
+++ datasets.py
@@ -4,6 +4,11 @@ from torch.utils.data import Dataset
 
 class CircleDataset(Dataset):
     def __init__(self):
+
+        self.width = 0.3
+        self.centerx = self.centery = self.center = 2
+        self.radius = self.center * 1.5
+
         self.xx = self.circle_data_generator()
 
     def __len__(self):
@@ -20,17 +25,14 @@ class CircleDataset(Dataset):
             2 - out circle
             radius = 0.8
         """
-        width = 0.3
-        centerx = centery = center = 2
-        radius = center * 1.5
-        xx = torch.randn(1000, 2)*radius + center
+        xx = torch.randn(1000, 2)*self.radius + self.center
 
         return xx
 
     def _func_y(self, point):
-        if (point[0] - centerx)**2 + (point[1] - centery) **2 < (radius-width)**2:
+        if (point[0] - self.centerx)**2 + (point[1] - self.centery) **2 < (self.radius-self.width)**2:
             return 0
-        elif (point[0] - centerx)**2 + (point[1] - centery) **2 < (radius+width)**2:
+        elif (point[0] - self.centerx)**2 + (point[1] - self.centery) **2 < (self.radius+self.width)**2:
             return 1
         else:
             return 2
