473d53374da72a286dffcce5fe9f489b1f0a2d0b
gaojx <gaojxcs@gmail.com>
2020-07-25 09:43:43 +0800

    use torch dataset

--- test_train.py
+++ test_train.py
@@ -6,9 +6,9 @@ import torch
 from torch import nn
 from torch.utils.tensorboard import SummaryWriter
 from torch.utils.data import random_split
+from torch.utils.data import Dataset, DataLoader
 
 from dql_model import QNet
-from dql_model import DataLoader
 from visualize_model import visualize_q_net
 from datasets import CircleDataset
 
@@ -90,15 +90,15 @@ def test_train_linear():
     plt.show()
 
 
-def create_data_mesh(xx):
+def create_data_mesh(ds: Dataset):
     range_x = (
-        xx[...,0].min(), 
-        xx[...,0].max()
+        ds.xx[...,0].min().item(), 
+        ds.xx[...,0].max().item()
     )
 
     range_y = (
-        xx[...,1].min(), 
-        xx[...,1].max()
+        ds.xx[...,1].min().item(), 
+        ds.xx[...,1].max().item()
     )
 
     X,Y = np.mgrid[
@@ -123,9 +123,8 @@ def test_draw_layer():
     q_net = QNet(2, 3)
     print("generating data")
     ds = CircleDataset()
-    xx = ds.xx
     print("creating data mesh")
-    mesh = create_data_mesh(xx)
+    mesh = create_data_mesh(ds)
 
     print("initialize gridspec")
     plot0 = plt.figure(0, figsize=(30, 30))
@@ -198,66 +197,82 @@ def test_train_circle():
     # generate data
     ds = CircleDataset()
 
-    mesh = create_data_mesh(ds.xx)
-    train_size = int(len(xx) * 0.8)
-    test_size = len(xx) - train_size
+    mesh = create_data_mesh(ds)
+    train_size = int(len(ds) * 0.8)
+    test_size = len(ds) - train_size
 
-    ds = random_split(torch.stack([xx, yy], -1, [train_size, test_size]))
-    # xx = ds[0].dataset[...,0]
-    # xx = ds[0].dataset[...,0]
-
-    # random split
-    random_split()
+    ds_tr, ds_te = random_split(ds, [train_size, test_size])
+    dl_tr = DataLoader(ds_tr, batch_size=32)
+    dl_te = DataLoader(ds_te, batch_size=32)
 
     # show circle data
     fig2 = plt.figure(2, figsize=(10, 10))
-    plt.scatter(xx[...,0], xx[...,1], c=yy)
+    for xx, yy in dl_tr:
+        plt.scatter(xx[...,0], xx[...,1], c=yy)
     plt.pause(0.1)
 
-    q_net = QNet(2, 3)
-
-    dl = DataLoader(xx, yy, batch_size=256)
+    q_net = QNet(5, 3)
 
     optimizer = torch.optim.SGD(q_net.parameters(), lr=0.001)
     loss_func = nn.NLLLoss()
 
     # init grids for nn visualization
     plot3, gs = create_gs(3, 10, 10)
-
     layer_ids = [1, 3, 5, 7]
     ax_mat = init_model_layers_axs(q_net, plot3, gs, layer_ids)
 
     print(q_net)
 
-    writer = SummaryWriter(log_dir="runs")
+    writer = SummaryWriter()
 
-    for i in range(10000):
-        x_batch, y_batch = dl.next_batch()
-        # plt.scatter(np.array(x_batch)[...,0], np.array(x_batch)[...,1], c=y_batch)
-        y_lsm = q_net(torch.Tensor(x_batch))
-        # y_pred = y_prob.argmax(dim=1)
+    max_epoches = 10000
 
-        loss = loss_func(y_lsm, torch.tensor(y_batch, dtype=torch.int64))
-        loss.backward()
-        optimizer.step()
-        writer.scalar('loss', loss)
+    loss_id = 0
+    for epoch in range(max_epoches):
+        print(f"epoch: {epoch}")
+        for xx, yy in dl_tr:
+            y_lsm = q_net(xx)
+            loss = loss_func(y_lsm, yy)
+            loss.backward()
+            optimizer.step()
+            writer.add_scalar('loss', loss, loss_id)
+            loss_id += 1
 
-        precision = precision_score(
-            y_batch, 
-            q_net(torch.Tensor(x_batch)).argmax(dim=-1), 
-            average='micro')
+        with torch.no_grad():
+            precision_list = []
+            for xx, yy in dl_te:
+                precision = precision_score(
+                    yy, 
+                    q_net(xx).argmax(dim=-1), 
+                    average='micro')
 
-        if i % 100 == 0:
-            print(i)
-            print(f"loss: {loss}")
-            print(f"precision: {precision}")
+                precision_list.append(precision)
+            precision = np.mean(precision_list)
+            writer.add_scalar('test_precision', precision, epoch)
 
-            draw_model_layers(q_net, mesh, ax_mat, layer_ids)
-            plt.pause(0.1)
+            precision_list = []
+            for xx, yy in dl_tr:
+                precision = precision_score(
+                    yy, 
+                    q_net(xx).argmax(dim=-1), 
+                    average='micro')
 
-            for axs in ax_mat:
-                for ax in axs:
-                    ax.cla()
+                precision_list.append(precision)
+            precision = np.mean(precision_list)
+            writer.add_scalar('train_precision', precision, epoch)
+
+
+        # if i % 100 == 0:
+        #     print(i)
+        #     print(f"loss: {loss}")
+        #     print(f"precision: {precision}")
+
+        #     draw_model_layers(q_net, mesh, ax_mat, layer_ids)
+        #     plt.pause(0.1)
+
+        #     for axs in ax_mat:
+        #         for ax in axs:
+        #             ax.cla()
 
 if __name__ == "__main__":
     test_train_circle()
