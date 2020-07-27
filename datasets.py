3d115a0a8adec45ed87eb4eafd5a096932cc5f48
gaojx <gaojxcs@gmail.com>
2020-07-28 07:59:22 +0800

    add binary circle ds

--- datasets.py
+++ datasets.py
@@ -1,11 +1,70 @@
 import matplotlib.pyplot as plt
 
+import numpy as np
 import torch
 from torch.utils.data import Dataset
 
 
 class BinaryCircleDataset(Dataset):
-    pass
+    def __init__(self, sample_num=1000):
+        self.sample_num = sample_num
+        self.radius = 1
+        self.radius1 = (0*self.radius, 0.5*self.radius)
+        self.radius2 = (0.7*self.radius, self.radius)
+        self.radius3 = (1.3*self.radius, 1.6*self.radius)
+        self.noise = 0.05
+
+        self.xx, self.yy = self.bi_circle_data_generator()
+
+    def __len__(self):
+        return len(self.xx)
+
+    def __getitem__(self, idx):
+        return self.xx[idx], self.yy[idx]
+        
+    def bi_circle_data_generator(self):
+        """
+        """
+        def _make_circle_data(point_num, radius, noise=self.noise):
+            r = np.random.uniform(radius[0], radius[1], point_num)
+            angles = np.random.uniform(0, 2 * np.pi, point_num)
+
+            xx = np.cos(angles) * r + np.random.uniform(
+                -noise*self.radius, 
+                noise*self.radius, point_num)
+            yy = np.sin(angles) * r + np.random.uniform(
+                -noise*self.radius, 
+                noise*self.radius, point_num)
+            return np.stack([xx, yy], axis=1)
+
+        inner_points = _make_circle_data(
+            self.sample_num // 3, 
+            self.radius1)
+
+        middle_points = _make_circle_data(
+            self.sample_num // 3, 
+            self.radius2)
+
+        outter_points = _make_circle_data(
+            self.sample_num // 3, 
+            self.radius3)
+
+        inner_yy = torch.tensor([0] * len(inner_points))
+        middle_yy = torch.tensor([1] * len(outter_points))
+        outter_yy = torch.tensor([2] * len(outter_points))
+
+        return torch.tensor(np.concatenate([inner_points, middle_points, outter_points]), dtype=torch.float), \
+            torch.cat([inner_yy, middle_yy, outter_yy])
+
+    def scatter(self):
+        xx, yy, cc = [], [], []
+        for xx_, yy_ in self:
+            xx.append(xx_[0])
+            yy.append(xx_[1])
+            cc.append(yy_)
+
+        plt.scatter(xx, yy, c=cc)
+
 
 class CircleDataset(Dataset):
     def __init__(self, sample_num=1000):
