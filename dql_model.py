cef2bf5f1f9c4c328c9deac50e2de87f890e5e69
gaojx <gaojxcs@gmail.com>
2020-07-30 07:28:31 +0800

    add deque

--- dql_model.py
+++ dql_model.py
@@ -1,8 +1,11 @@
+import collections
 import random
 
 import numpy as np
 import torch
 from torch import nn
+
+import config
 
 
 class QNet(nn.Module):
@@ -18,8 +21,7 @@ class QNet(nn.Module):
             nn.Tanh(), 
             nn.Linear(20, 30), 
             nn.Tanh(), 
-            nn.Linear(30, num_classes), 
-            nn.LogSoftmax(dim=-1)
+            nn.Linear(30, num_classes)
         )
 
     def forward(self, x):
@@ -33,16 +35,21 @@ class ModelTranner:
         self.optimizer = torch.optim.SGD(self.q_net.parameters(), lr=lr)
 
     def compute_loss(self, xx, yy, q_hat):
-        S = np.concatenate([xxx[0] for xxx in xx]).reshape(-1, 2)
-        A = torch.tensor([xxx[1] for xxx in xx], dtype=torch.int64)
+        S = torch.tensor(
+            np.concatenate(
+                [xxx[0] for xxx in xx]).reshape(-1, 2), dtype=torch.float).to(config.DEVICE)
+        A = torch.tensor([xxx[1] for xxx in xx], dtype=torch.int64).to(config.DEVICE)
 
-        prob_y = self.q_net(torch.Tensor(S))
+        prob_y = self.q_net(S)
         pred_y = prob_y.gather(
             1, A.unsqueeze(-1)
         ).squeeze(-1)
 
         true_y = self.compute_target(xx, yy, q_hat)
 
+        # print(f"prob_y: {prob_y}")
+        # print(f"pred_y: {pred_y}")
+        # print(f"true_y: {true_y}")
         return self.loss_func(pred_y, true_y)
 
     def compute_target(self, xx, yy, q_hat):
@@ -51,32 +58,39 @@ class ModelTranner:
         y = R + gamma * max(Q_hat(S))
                             m
         """
-        q_gamma = 0.95
-        Sp = np.concatenate([xxx[3] for xxx in xx]).reshape(-1, 2)
-        m = q_hat(torch.Tensor(Sp)).max(dim=1)
-        masked_m = torch.Tensor(yy)* m.values
-        R = torch.Tensor([xxx[2] for xxx in xx])
-        Y = R + q_gamma * masked_m
+        with torch.no_grad():
+            q_gamma = 0.95
+            Sp = torch.tensor(np.concatenate([xxx[3] for xxx in xx]).reshape(-1, 2), 
+                dtype=torch.float).to(config.DEVICE)
+            Sp_v = q_hat(Sp).max(dim=1)
+            masked_Sp_v = torch.Tensor(yy).to(config.DEVICE)* Sp_v.values
+            masked_Sp_v = masked_Sp_v.detach()
+            R = torch.Tensor([xxx[2] for xxx in xx]).to(config.DEVICE)
+            Y = R + q_gamma * masked_Sp_v
         return Y
 
     def train(self, x_batch, y_batch, q_hat):
-        print(f"size of x in train: {len(x_batch)}")
+        # print(f"size of x in train: {len(x_batch)}")
         self.optimizer.zero_grad()
         loss = self.compute_loss(x_batch, y_batch, q_hat)
         loss.backward()
-        print(f"loss: {loss}")
+        # print(f"loss: {loss}")
 
         self.optimizer.step()
 
+        return loss, [params.grad for params in self.q_net.parameters()]
+
 
 class DataLoader:
-    def __init__(self, x=None, y=None, batch_size=32):
+    def __init__(self, x=None, y=None, batch_size=32, memory_size=2000):
+        self.memory_size = memory_size
+
         if x is None and y is None:
-            self.data_x = []
-            self.data_y = []
-        else:
-            self.data_x = x
-            self.data_y = y
+            self.data_x = collections.deque(maxlen=self.memory_size)
+            self.data_y = collections.deque(maxlen=self.memory_size)
+        # else:
+        #     self.data_x = x
+        #     self.data_y = y
 
         self.batch_size = batch_size
 
@@ -85,8 +99,8 @@ class DataLoader:
         self.data_y.append(y)
 
     def clear(self):
-        self.data_x = []
-        self.data_y = []
+        self.data_x = collections.deque(maxlen=self.memory_size)
+        self.data_y = collections.deque(maxlen=self.memory_size)
 
     def next_batch(self):
         idxs = random.sample(range(len(self.data_x)), self.batch_size)
