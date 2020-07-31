51817bfe8268a9fa4401a974ec0f7afdabc93ebe
gaojx <gaojxcs@gmail.com>
2020-07-31 18:12:44 +0800

    fix state update error

--- simulation.py
+++ simulation.py
@@ -7,7 +7,7 @@ import config
 
 
 class EnvRunner:
-    def __init__(self, env, history:DataLoader, q_hat, epsilon):
+    def __init__(self, env, history:DataLoader, q_hat, epsilon, writer):
         self.env = env
         self.history = history
         self.q_hat = q_hat
@@ -15,40 +15,59 @@ class EnvRunner:
         self.init_epsilon()
 
         self.total_round = 0
+        self.max_pos = -999
+        self.writer = writer
 
     def run_one_step(self, state_t):
         # check epsilon
         if np.random.uniform(0, 1) > self.epsilon:
             with torch.no_grad():
-                action = self.q_hat.forward(
+                action_v = self.q_hat.forward(
                         torch.Tensor(np.expand_dims(state_t, 0)).to(config.DEVICE)
-                    ).argmax().item()
+                    )
+                action = action_v.argmax().item()
+
+                self.writer.add_scalar("action", 
+                    action)
+                self.writer.add_scalars("action_v", {
+                    "action_v0": action_v[0][0], 
+                    "action_v1": action_v[0][1], 
+                    "action_v2": action_v[0][2], 
+                    })
         else:
+            # random action
             action = self.env.action_space.sample()
 
         # take one step
         state_tp1, reward, done, _ = self.env.step(action)
 
         # change reward
-        if state_tp1[0] > 0.5:  # pass flag
+        pos = state_tp1[0]
+        if pos >= 0.5:  # pass flag
             reward = 0
+            print("reach final")
+        #elif pos > self.max_pos:
+        #    print(f"exceed max pos! {self.max_pos} -> {pos}")
+        #    reward = (pos - self.max_pos) * 10
+        #    self.max_pos = pos
 
         # save state (s, a, r, s’) into {replay}
         if not done:
             self.history.append(
                 (state_t, action, reward, state_tp1), 1)
         else:
+            state_tp1 = state_t
             self.history.append(
                 (state_t, action, reward, state_t), 0)
 
-        return done
+        return done, state_tp1
 
     def run_one_episode(self, episode):
         done = False
         state_t = self.env.reset()
 
         while not done:
-            done = self.run_one_step(state_t)
+            done, state_t = self.run_one_step(state_t)
 
         self.total_round += 1
 
@@ -60,8 +79,8 @@ class EnvRunner:
     def init_epsilon(self):
         self.epsilon_restore_period = 1000
         self.epsilon_restore = 0.2
-        self.epsilon_min = 0.05
-        self.epsilon_decay = 0.9
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.99
 
     def epsilon_update(self, episode):
         # restore epsilon periodically
@@ -72,14 +91,14 @@ class EnvRunner:
 
         # decay epsilon
         new_epsilon = self.decay_epsilon()
-        if new_epsilon and self.total_round % 30 == 29:
-            print(f"epsilon decayed to {new_epsilon}")
+        #if new_epsilon and self.total_round % 30 == 29:
+        print(f"epsilon decayed to {new_epsilon}")
 
     def decay_epsilon(self):
         if self.epsilon > self.epsilon_min:
             self.epsilon = self.epsilon * self.epsilon_decay
             return self.epsilon
         else:
-            return None
+            return self.epsilon
 
 
